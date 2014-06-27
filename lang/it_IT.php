6e6ed3fb8db2f4c56fe80d84d4e0716295c4ff0d
Michael Moroni <michael.moroni@mailoo.org>
2014-06-27 09:37:08 +0200

    Create it_IT.php
    Initial Italian translation. Part. 1

---
+++ lang/it_IT.php
@@ -0,0 +1,295 @@
+<?php
+//PANELS
+define('PANEL_EAT','Cibo');
+define('PANEL_PARTY','Divertimento');
+define('PANEL_BUY','Spesa');
+define('PANEL_EXERCISE','Sport');
+define('PANEL_OFFICE','Uffici');
+define('PANEL_CRAFT','Craft');
+define('PANEL_NEED','Necessità');
+define('PANEL_EMERGENCY', 'Emergenza');
+define('PANEL_MONEY','Soldi');
+define('PANEL_ALL', 'Tutto');
+
+define('PANEL_CUISINE','Cucina');
+define('PANEL_CUISINE_ALL','Tutto');
+define('PANEL_CUISINE_BAGEL','Bagel');
+define('PANEL_CUISINE_BARBECUE','Barbecue');
+define('PANEL_CUISINE_BOUGATSA','Bougatsa');
+define('PANEL_CUISINE_BURGER','Burger');
+define('PANEL_CUISINE_CAKE','Torta');
+define('PANEL_CUISINE_CHICKEN','Pollo');
+define('PANEL_CUISINE_CAFFEE_SHOP','Cafè');
+define('PANEL_CUISINE_CREPE','Crépe');
+define('PANEL_CUISINE_COUSCOUS','Couscous');
+define('PANEL_CUISINE_CURRY','Curry');
+define('PANEL_CUISINE_DONUT','Donut'); //FIXME
+define('PANEL_CUISINE_DOUGHNUT','Doughnut'); //FIXME
+define('PANEL_CUISINE_EMPANADA','Empanada');
+define('PANEL_CUISINE_FISH_AND_CHIPS','Fish and chips');
+define('PANEL_CUISINE_FRIED_FOOD','Fritto');
+define('PANEL_CUISINE_FRITURE','Frittura');
+define('PANEL_CUISINE_ICE_CREAM','Gelato');
+define('PANEL_CUISINE_KEBAB','Kebab');
+define('PANEL_CUISINE_MEDITERRANEAN','Mediterraneo');
+define('PANEL_CUISINE_NOODLE','Noodle');
+define('PANEL_CUISINE_PANCAKE','Pancake');
+define('PANEL_CUISINE_PASTA','Pasta');
+define('PANEL_CUISINE_PIE','Torta di mele');
+define('PANEL_CUISINE_PIZZA','Pizza');
+define('PANEL_CUISINE_REGIONAL','Regionale');
+define('PANEL_CUISINE_SANDWICH','Sandwich');
+define('PANEL_CUISINE_SAUSAGE','Salsiccia');
+define('PANEL_CUISINE_SAVORY_PANCAKES','Pancake salato');
+define('PANEL_CUISINE_SEAFOOD','Frutti di mare');
+define('PANEL_CUISINE_STEAK_HOUSE','Steak house');
+define('PANEL_CUISINE_SUSHI','Sushi');
+define('PANEL_CUISINE_AFRICAN','Africano');
+define('PANEL_CUISINE_AMERICAN','Americano');
+define('PANEL_CUISINE_ARAB','Arabo');
+define('PANEL_CUISINE_ARGENTINIAN','Argentino');
+define('PANEL_CUISINE_ASIAN','Asiatico');
+define('PANEL_CUISINE_BAIANA','Baiano');
+define('PANEL_CUISINE_BALKAN','Balcano');
+define('PANEL_CUISINE_BASQUE','Basco');
+define('PANEL_CUISINE_BAVARIAN','Bavarese');
+define('PANEL_CUISINE_BRAZILIAN','Brasiliano');
+define('PANEL_CUISINE_CANTONESE','Cantonese');
+define('PANEL_CUISINE_CAPIXABA','Capixaba');
+define('PANEL_CUISINE_CARIBBEAN','Caraibico');
+define('PANEL_CUISINE_CHINESE','Cinese');
+define('PANEL_CUISINE_CROATIAN','Croato');
+define('PANEL_CUISINE_CZECH','Ceco');
+define('PANEL_CUISINE_FRENCH','Francese');
+define('PANEL_CUISINE_GERMAN','Tedesco');
+define('PANEL_CUISINE_GREEK','Greco;
+define('PANEL_CUISINE_GAUCHO','Gaucho');
+define('PANEL_CUISINE_HUNAN','Hunan');
+define('PANEL_CUISINE_HUNGARIAN','Ungherese');
+define('PANEL_CUISINE_INDIAN','Indiano');
+define('PANEL_CUISINE_INTERNATIONAL','Internazionale');
+define('PANEL_CUISINE_IRANIAN','Iraniano');
+define('PANEL_CUISINE_ITALIAN','Italiano');
+define('PANEL_CUISINE_JAPANESE','Giapponese');
+define('PANEL_CUISINE_KYO_RYOURI','Kyo ryouri');
+define('PANEL_CUISINE_KOREAN','Coreano');
+define('PANEL_CUISINE_LATIN_AMERICAN','Latino-americano');
+define('PANEL_CUISINE_LEBANESE','Libanese');
+define('PANEL_CUISINE_MEXICAN','Messicano');
+define('PANEL_CUISINE_MINEIRA','Mineira');
+define('PANEL_CUISINE_OKINAWA_RYORI','Okinawa ryori');
+define('PANEL_CUISINE_PERUVIAN','Peruviano');
+define('PANEL_CUISINE_POLISH','Polacco');
+define('PANEL_CUISINE_PORTUGUESE','Portoghese');
+define('PANEL_CUISINE_RUSSIAN','Russo');
+define('PANEL_CUISINE_SHANDONG','Shandong');
+define('PANEL_CUISINE_SICHUAN','Sichuan');
+define('PANEL_CUISINE_SPANISH','Spagnolo');
+define('PANEL_CUISINE_THAI','Thailandese');
+define('PANEL_CUISINE_TURKISH','Turco');
+
+define('PANEL_DIET','Dieta');
+define('PANEL_DIET_HEADER','Non selezionato/Sì/Solo');
+define('PANEL_DIET_PESCETARIAN','Pescetariano');
+define('PANEL_DIET_VEGETARIAN','Vegetariano');
+define('PANEL_DIET_LACTO_VEGETARIAN','Lacto vegetariano');
+define('PANEL_DIET_OVO_VEGETARIAN','Ovo vegetariano');
+define('PANEL_DIET_VEGAN','Vegan');
+define('PANEL_DIET_FRUITARIAN','Fruttariano');
+define('PANEL_DIET_RAW','Crudista');
+define('PANEL_DIET_GLUTEN_FREE','Senza glutine');
+define('PANEL_DIET_DAIRY_FREE','Senza prodotti caseari');
+define('PANEL_DIET_LACTOSE_FREE','Senza lattosio');
+define('PANEL_DIET_HALAL','Halal');
+
+define('PANEL_PLACE_ALL','Tutto');
+define('PANEL_PLACE_ATM','Bancomat');
+define('PANEL_PLACE_BANK','Banca');
+
+define('PANEL_WHERE','Dove');
+define('PANEL_WHERE_HEADER','Escludi');
+define('PANEL_WHERE_PUB','Pub');
+define('PANEL_WHERE_BAR','Bar');
+define('PANEL_WHERE_NIGHTCLUB','Nightclub');
+define('PANEL_WHERE_BIERGARTEN','Biergarten');
+define('PANEL_WHERE_STRIPCLUB','Stripclub');
+define('PANEL_WHERE_SPORT_CENTRE','Centro sportivo');
+define('PANEL_WHERE_PITCH','Campo');
+define('PANEL_WHERE_STADIUM','Stadio');
+define('PANEL_WHERE_SWIMMING_POOL','Piscina');
+define('PANEL_WHERE_TRACK','Traccia');
+define('PANEL_WHERE_OTHER','Altro');
+
+define('PANEL_BEER','Birra');
+define('PANEL_BEER_HEADER','Seleziona');
+define('PANEL_BEER_MICROBREWERY','Microbirrificio');
+define('PANEL_BEER_REAL_CIDER','Sidro vero');
+define('PANEL_BEER_REAL_ALE','Vera ale');
+define('PANEL_BEER_BREWERY_PLACEHOLDER','es. Heineken');
+
+// FIXME
+define('PANEL_OFFICE_LIST','Tipo');
+define('PANEL_OFFICE_ALL','Tutto');
+define('PANEL_OFFICE_ACCOUNTANT','Accountant');
+define('PANEL_OFFICE_ADMINISTRATIVE','Administrative');
+define('PANEL_OFFICE_ARCHITECT','Architect');
+define('PANEL_OFFICE_ASSOCIATION','Association');
+define('PANEL_OFFICE_LAWYER','Lawyer');
+define('PANEL_OFFICE_NOTARY','Notary');
+
+// FIXME
+define('PANEL_CRAFT_LIST','Tipo');
+define('PANEL_CRAFT_ALL','Tutto');
+define('PANEL_CRAFT_CARPENTER','Carpenter');
+define('PANEL_CRAFT_CLOCKMAKER','Clockmaker');
+define('PANEL_CRAFT_GLAZIERY','Glaziery');
+define('PANEL_CRAFT_PHOTOGRAPHER','Photographer');
+
+define('PANEL_NEED_LIST','Tipo');
+define('PANEL_NEED_HEADER','Includi');
+define('PANEL_NEED_TOILETS','Bagni');
+define('PANEL_NEED_DRINKING_WATER','Fontanelle');
+define('PANEL_NEED_SHELTER','Rifugio');
+
+define('PANEL_EMERGENCY_LIST', 'Lista');
+define('PANEL_EMERGENCY_LIST_HEADER', 'Includi');
+define('PANEL_EMERGENCY_DEFIBRILLATOR', 'Defibrillatore');
+
+define('PANEL_ACCESS','Accesso');
+define('PANEL_ACCESS_HEADER','Non selezionato/Sì/Solo');
+define('PANEL_ACCESS_MALE','Uomo');
+define('PANEL_ACCESS_FEMALE','Donna');
+define('PANEL_ACCESS_GAY','Gay');
+
+// FIXME
+define('PANEL_STORE','Store');
+define('PANEL_STORE_ALL','Tutto');
+define('PANEL_STORE_ALCOHOL','Alcohol');
+define('PANEL_STORE_ART','Art');
+define('PANEL_STORE_CLOTHES','Clothes');
+define('PANEL_STORE_FOOD','Food');
+define('PANEL_STORE_ELECTRONIC','Electronic');
+define('PANEL_STORE_HEALTH_AND_BEAUTY','Health and beauty');
+define('PANEL_STORE_TRANSPORT','Transport');
+define('PANEL_STORE_TRAVEL','Travel');
+
+// FIXME
+define('PANEL_SPORT','Sport');
+define('PANEL_SPORT_ALL','All');
+define('PANEL_SPORT_9PIN','9Pin');
+define('PANEL_SPORT_10PIN','10Pin');
+define('PANEL_SPORT_AMERICAN_FOOTBALL','American football');
+define('PANEL_SPORT_AIKIDO','Aikido');
+define('PANEL_SPORT_ARCHERY','Archery');
+define('PANEL_SPORT_ATHLETICS','Athletics');
+define('PANEL_SPORT_AUSTRALIAN_FOOTBALL','Australian football');
+define('PANEL_SPORT_BASE','Base');
+define('PANEL_SPORT_BADMINTON','Badminton');
+define('PANEL_SPORT_BASEBALL','Baseball');
+define('PANEL_SPORT_BASKETBALL','Basketball');
+define('PANEL_SPORT_BEACHVOLLEYBALL','Beachvolleyball');
+define('PANEL_SPORT_BMX','BMX');
+define('PANEL_SPORT_BOULES','Boules');
+define('PANEL_SPORT_BOWLS','Bowls');
+define('PANEL_SPORT_BOXING','Boxing');
+define('PANEL_SPORT_CANADIAN_FOOTBALL','Canadian football');
+define('PANEL_SPORT_CANOE','Canoe');
+define('PANEL_SPORT_CHESS','Chess');
+define('PANEL_SPORT_CLIMBING','Climbing');
+define('PANEL_SPORT_CRICKET','Cricket');
+define('PANEL_SPORT_CRICKET_NETS','Cricket nets');
+define('PANEL_SPORT_CROQUET','Croquet');
+define('PANEL_SPORT_CYCLING','Cycling');
+define('PANEL_SPORT_DIVING','Diving');
+define('PANEL_SPORT_DOG_RACING','Dog racing');
+define('PANEL_SPORT_FENCING','Fencing');
+define('PANEL_SPORT_EQUESTRIAN','Equestrian');
+define('PANEL_SPORT_FREE_FLYING','Free flying');
+define('PANEL_SPORT_GAELIC_GAMES','Gaelic games');
+define('PANEL_SPORT_GOLF','Golf');
+define('PANEL_SPORT_GYMNASTICS','Gymnastics');
+define('PANEL_SPORT_HOCKEY','Hockey');
+define('PANEL_SPORT_HORSESHOES','Horseshoes');
+define('PANEL_SPORT_HORSE_RACING','Horse racing');
+define('PANEL_SPORT_ICE_STOCK','Ice stock');
+define('PANEL_SPORT_JUDO','Judo');
+define('PANEL_SPORT_KARTING','Karting');
+define('PANEL_SPORT_KITESURFING','Kitesurfing');
+define('PANEL_SPORT_KORFBALL','Korfball');
+define('PANEL_SPORT_MOTOR','Motor');
+define('PANEL_SPORT_OBSTACLE_COURSE','Obstacle course');
+define('PANEL_SPORT_ORIENTEERING','Orienting');
+define('PANEL_SPORT_PADDLE_TENNIS','Paddle tennis');
+define('PANEL_SPORT_PARAGLIDING','Paragliding');
+define('PANEL_SPORT_PALOTA','Palota');
+define('PANEL_SPORT_RACQUET','Racquet');
+define('PANEL_SPORT_ROWING','Rowing');
+define('PANEL_SPORT_RUGBY_LEAGUE','Rugby league');
+define('PANEL_SPORT_RUGBY_UNION','Rugby union');
+define('PANEL_SPORT_RUNNING','Running');
+define('PANEL_SPORT_SCUBA_DIVING','Scuba diving');
+define('PANEL_SPORT_SHOOTING','Shooting');
+define('PANEL_SPORT_SKATING','Skating');
+define('PANEL_SPORT_SKATEBOARD','Skateboard');
+define('PANEL_SPORT_SKIING','Skiing');
+define('PANEL_SPORT_SOCCER','Soccer');
+define('PANEL_SPORT_SURFING','Surfing');
+define('PANEL_SPORT_SWIMMING','Swimming');
+define('PANEL_SPORT_TABLE_TENNIS','Table tennis');
+define('PANEL_SPORT_TEAKWONDO','Teakwondo');
+define('PANEL_SPORT_TEAM_HANDBALL','Team handball');
+define('PANEL_SPORT_TENNIS','Tennis');
+define('PANEL_SPORT_TOBOGGAN','Toboggan');
+define('PANEL_SPORT_VALLEYBALL','Valleyball');
+define('PANEL_SPORT_WATER_SKI','Water ski');
+define('PANEL_SPORT_WEIGHTLIFTING','Weightlifting');
+define('PANEL_SPORT_WRESTLING','Wrestling');
+
+// FIXME
+define('PANEL_OTHER','Other');
+define('PANEL_OTHER_HEADER','No set/Yes');
+define('PANEL_OTHER_TAKE_AWAY','Take away');
+define('PANEL_OTHER_DELIVERY','Delivery');
+define('PANEL_OTHER_WHEELCHAIR','Wheelchair');
+define('PANEL_OTHER_SMOKING','Smoking');
+define('PANEL_OTHER_INTERNET','Internet access');
+define('PANEL_FREE','Free');
+define('PANEL_WLAN','Wlan');
+
+// FIXME
+define('PANEL_EMPTY','Empty');
+define('PANEL_NO','No');
+define('PANEL_YES','Yes');
+define('PANEL_DEDICATED','Dedicated');
+define('PANEL_ISOLATED','Isolated');
+define('PANEL_SEPARATED','Separated');
+
+// FIXME
+define('BUTTON_SET','Set');
+define('BUTTON_LOCATE','Locate');
+define('BUTTON_STOP_LOCATE','Stop locate');
+define('BUTTON_ABOUT','About');
+define('POPUP_ABOUT_BODY','Data from <a href="http://osm.org">OpenStreetMap</a> ODbL. <a href="http://en.wikipedia.org/wiki/OpenStreetMap">About</a><br/>Between 10-15 zoom is downloaded only 40POI. Fulldata minzoom=15.<br/>Key:<br/><span  class="glyphicon glyphicon-ok">yes</span><br/><span  class="glyphicon glyphicon-ok-sign">only</span><br/><br/>Used libs and tools:<br/><a href="http://leafletjs.com">Leaflet</a>, <a href="http://getbootstrap.com">Bootstrap</a>, <a href="http://jquery.com/">jQuery</a>, <a href="https://github.com/AMDmi3/opening_hours.js">Opening_hours</a>, <a href="https://github.com/mlevans/leaflet-hash">Leaflet-hash</a>, <a href="https://github.com/kartenkarsten/leaflet-layer-overpass/">LeafLet-layer-overpass</a>, Overpass API, VIM :-)<br/>Icons: Maps Icons Collection <a href="http://mapicons.nicolasmollet.com">http://mapicons.nicolasmollet.com</a><br/>Author: Adam Jordanek (dotevo)');
+define('BUTTON_CLOSE','Close');
+define('BUTTON_ADD','Add');
+
+// FIXME
+define('JS_ADD_NOTE_BODY','You can add a note for mappers. Reported data will be added in a few weeks. You can do it yourself! Go to the <a href="http://openstreetmap.org">OpenStreetMap</a> page and edit. Use of English or the national language. Note should be understandable for mappers.');
+define('JS_ADD_NOTE_LIST','Added notes:');
+define('PLEASE_WAIT','Please wait...');
+define('JS_ADD_NOTE_LABEL','Note');
+define('JS_ADD_NOTE_PLACEHOLDER','Some important information');
+
+// FIXME
+define('JS_NOT_FOUND','Not Found');
+define('JS_ADD_MISSING_DATA','Add missing data!');
+define('JS_REPORT','Report');
+define('JS_KEY','Key');
+define('JS_VALUE','Value');
+define('JS_OPENING_HOURS','Opening hours');
+define('JS_BASIC','Basic');
+define('JS_NOTE','Note');
+define('JS_STATUS','Status');
+?>        
+          
+          

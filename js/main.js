3470bce4308538305c7ff546cfd789178e49cb22
dotevo <dotevo@gmail.com>
2014-10-01 21:32:07 +0200

    Leaflet map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
-#ifdef debugMode
-    var myFunc = function(a,b) {
-        // body of your function here
-    }
-#else
-    var myFunc = function(a,b) {
-        // body of your alternate function here
-    }
-#endif
+var i=true;
+var map;
+$("#bottomheader").on("click", function () {
+	if(i)
+		$( "#content" ).fadeTo( 400, 0.5 );
+	else
+		$( "#content" ).fadeTo( 400, 1 );
+	i=!i;
+	$("#bottomcontent").slideToggle(200);
+});
+
+$(document).on('pagebeforeshow', '#page', function(){
+	$( "#left-panel" ).panel( "open");
+});
+
+$( document ).ready(function() {
+	map = L.map('map').setView([51.505, -0.09], 13);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+		maxZoom: 18
+	}).addTo(map);
+	$(window).load(function() {
+		map.invalidateSize();
+	});
+});

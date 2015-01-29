43743a88c04e0a47d94251306c2fd15b5e7e7aa5
dotevo <dotevo@gmail.com>
2015-01-29 15:59:33 +0100

    Translation: DE
    fix #38

--- main.php
+++ main.php
@@ -568,6 +568,7 @@ include("language.php");
               <li><a href="?lang=it_IT"><img src="img/flags/it_IT.png"/></a></li>
               <li><a href="?lang=ru_RU"><img src="img/flags/ru_RU.png"/></a></li>
               <li><a href="?lang=uk_UA"><img src="img/flags/uk_UA.png"/></a></li>
+              <li><a href="?lang=de_DE"><img src="img/flags/de_DE.png"/></a></li>
             </ul>
           </li>
           <li><button onclick="ustaw()" type="button" class="btn btn-primary navbar-btn"><?php echo BUTTON_SET;?></button></li>

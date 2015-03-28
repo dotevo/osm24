67a0d4664d29f2f633d6b5c96bbbdbc98d148f70
Nicolas Hohm <nickel7152@gmail.com>
2015-03-28 10:58:22 +0100

    Disable scale

--- main.php
+++ main.php
@@ -6,7 +6,7 @@ include("language.php");
 <html lang="<?php echo $_SESSION['slang']; ?>">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="fragment" content="!">
